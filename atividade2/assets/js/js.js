53934c57b9a26a73f6a187c97694a49e636f7951
Samuel-Prado <samuel.prado@estudante.iftm.edu.br>
2023-10-15 18:01:06 -0300

    te

--- atividade2/assets/js/js.js
+++ atividade2/assets/js/js.js
@@ -1,31 +1,21 @@
 $(document).ready(() => {
-    
-    $("#seletor").change(() => {
+
+    carregarReg();
+
+    function carregarReg() {
         const link = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes";
         $.getJSON(link, (regiao) => {
-            let norepet = "no";
-            
             if (regiao.erro === true) {
                 $("#mensagem").css("background-color", "tomato")
-                $("#mensagem").html("Estado não encontrado")
-
-            }else {
-                $("#mensagem").css("background-color", "#b7d5ac")
-                $("#mensagem").html("Os dados solicitados estão disponíveis a seguir.")
-             
-            if (norepet == "si") {
-                alert("oi")
-            }else{
-                norepet = "si";
+                $("#mensagem").html("Erro no servidor")
+            } else {
+                let text = "";
                 for (let i = 0; i < regiao.length; i++) {
-                $("#seletor").append("<option value="+ regiao[i].sigla +">"+regiao[i].sigla+"</option>")
-                    
+                    text += `<option value="${regiao[i].sigla}">${regiao[i].sigla}</option>`;
                 }
+                $("#seletor").append(text);
             }
-            }
-
         })
+    }
 
-
-    })
 })
